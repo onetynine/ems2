7f6b91cabd9a0853f05faf189f6b7e1cd8109be5
onetynine <aziziseers@gmail.com>
2023-12-22 17:54:38 +0800

    22/12/2023

--- emp_register.php
+++ emp_register.php
@@ -8,89 +8,16 @@ require 'conn.php';
        
        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
-  <br>      
+  <br> 
+  <!-- Add this div to show live feedback -->
+<div id="emailFeedback"></div>
+     
 <h3>Registration</h3><hr>
-<form id="myForm" action="emp_register_process.php" method="post">
-
-<!-- 
-Multi step registration js  is based on 
-https://codepen.io/aniket/pen/WxByVp 
--->
-<style>
-    .stepwizard-row {
-      display: table-row;
-  }
-
-  .stepwizard {
-      display: table;
-      width: 100%;
-      position: relative;
-  }
-
-  .stepwizard-step button[disabled] {
-      opacity: 1 !important;
-      filter: alpha(opacity=50) !important;
-  }
-
-  .stepwizard-row:before {
-      top: 14px;
-      bottom: 0;
-      position: absolute;
-      content: " ";
-      width: 100%;
-      height: 1px;
-      background-color: #ccc;
-      z-order: 0;
-
-  }
-
-  .stepwizard-step {
-      display: table-cell;
-      text-align: center;
-      position: relative;
-  }
-
-</style>
-
-<br>
-<div class="stepwizard setup-panel d-flex flex-row align-items-center text-center m-2">
-    <div class="stepwizard-step m-auto ">
-        <a href="#step-1" type="button" class="btn btn-sm btn-primary btn-circle">1 - Personal Info</a>
-        
-    </div>
-    <div class="stepwizard-step m-auto">
-        <a href="#step-2" type="button" class="btn btn-sm btn-default btn-circle" disabled>2 - Employment Info</a>
-    </div>
-    <div class="stepwizard-step m-auto">
-        <a href="#step-3" type="button" class="btn btn-sm btn-default btn-circle" disabled>3 - Leave Info</a>
-    </div>
-
-</div><br>
+<form action="emp_register_process.php" method="post" class="needs-validation" novalidate>
 
 <?php 
 
 ?>
-<div class="progress"  style="height: 2px" role="progressbar" aria-label="Animated striped example" aria-valuenow="00" aria-valuemin="00" aria-valuemax="00" id="progress-bar">
-  <div class="progress-bar progress-bar-striped progress-bar-animated w-100"></div>
-</div><br>
-
-<script>
-  // Function to update the progress bar based on the active step
-  function updateProgressBar() {
-    var totalSteps = 3; // Total number of steps
-    var activeStep = document.querySelector('.stepwizard-step a.btn-primary');
-
-    if (activeStep) {
-      var stepNumber = parseInt(activeStep.textContent.split(' ')[0]);
-      var percentage = (stepNumber / totalSteps) * 100;
-      document.getElementById('progress-bar').style.width = percentage + '%';
-    }
-  }
-
-  // Call the function initially and whenever the step changes
-  updateProgressBar();
-  document.addEventListener('click', updateProgressBar);
-</script>
 
 <!-- Personal Info Section --> 
 <section class="text-start row setup-content" id="step-1">
@@ -102,38 +29,45 @@ https://codepen.io/aniket/pen/WxByVp
   </div>
 <div class="card-body">
 <div class="form-floating mb-3">
-        <input type="text" class="form-control" id="emp_name" placeholder="emp_name" name="emp_name" required>
-        <small id="nameError" style="color: red;"></small>
+        <input type="text" class="form-control" id="emp_name" placeholder="emp_name" name="emp_name" required>       
         <label for="emp_name">Name</label>
+        <div class="invalid-feedback">
+        Please type a valid name.
+        </div>
     </div>
 
     <div class="form-floating mb-3">
-        <input type="text" class="form-control" id="emp_nric" placeholder="NRIC" name="emp_nric" required>
-        <small id="nricError" style="color: red;"></small>
+        <input type="text" class="form-control" id="emp_nric" placeholder="NRIC" name="emp_nric" required>   
         <label for="emp_nric">NRIC</label>
+        <div class="invalid-feedback">
+        Please type a valid IC Number (eg. 880818-08-8888).
+        </div>
     </div>
 
     <div class="form-floating mb-3">
         <input type="tel" class="form-control" id="emp_phone" placeholder="phone" name="emp_phone" required>
         <label for="emp_phone">Phone</label>
-        <small id="phoneError" style="color: red;"></small>
+        
     </div>
   
     <div class="form-floating mb-3">
-        <input type="email" class="form-control" id="emp_email" placeholder="name@example.com" name="emp_email" required>
-        <small id="emailError" style="color: red;"></small>
-        <label for="emp_email">Email address</label>
+    <input type="email" class="form-control <?php echo isset($emp_email_error) ? 'is-invalid' : ''; ?>" id="emp_email" placeholder="name@example.com" name="emp_email" required value="<?php echo isset($emp_email) ? $emp_email : ''; ?>">
+    <label for="emp_email">Email address</label>
+    <div class="invalid-feedback">
+        Please type a valid email address (eg. email@gmail.com)
+        </div>
     </div>
+
+
     <div class="form-check form-switch">
   <input class="form-check-input" type="checkbox" role="switch" name="emp_admin_access" id="emp_admin_access">
   <label class="form-check-label" for="emp_admin_access">Admin Access</label>
 </div>
 <br>
-    <button class="btn btn-primary nextBtn pull-right" type="button">Next</button>
 
 </div></div></div>
 
-</section>
+</section><br>
     <!-- Employment Info Section --> 
     <section class="text-start row setup-content" id="step-2">
     <div class="tab">
@@ -151,7 +85,7 @@ https://codepen.io/aniket/pen/WxByVp
         </div>
         <div class="form-floating mb-3">
         <select class="form-select" id="empDepartment" name="emp_department" required>
-            <option value="option1"></option>
+            <option value="" selected disabled>Select Department</option>
             <option value="option2">Information Technology</option>
             <option value="option3">Operation</option>
             <option value="option4">Logistic</option>
@@ -161,7 +95,7 @@ https://codepen.io/aniket/pen/WxByVp
 
         <div class="form-floating mb-3">
         <select class="form-select" id="emp_contract_type" name="emp_contract_type" required>
-            <option value="option1"></option>
+            <option value="" selected disabled>Select Contract Type</option>
             <option value="option2">Full Time</option>
             <option value="option3">Part Time</option>
             <option value="option4">Contract Based</option>
@@ -180,21 +114,15 @@ https://codepen.io/aniket/pen/WxByVp
             </div>
 
         </div>
-
-
         <div class="form-floating mb-3">
             <input type="text" class="form-control" id="floatingInput" placeholder="status" name="emp_status" required>
             <label for="floatingInput">Status</label>
         </div>
-    
-    <button class="btn btn-default prevBtn pull-left" type="button" >Prev</button>
-    <button class="btn btn-primary nextBtn pull-right" type="button" >Next</button>
 </div>
 </div>
-</section>
+</section><br>
 
-        <!-- Social Media Info Section --> 
-        <section class="text-start row setup-content" id="step-3">    
+    <section class="text-start row setup-content" id="step-3">    
     <br>
     <div class="tab">
     <div class="card" style="width: 100%;">
@@ -213,7 +141,7 @@ https://codepen.io/aniket/pen/WxByVp
       <p class="card-text">Set amount of annual leave.</p>
         <div class="col-md">
             <div class="form-floating mb-3">
-                    <input type="number" class="form-control" id="floatingInput" placeholder="emp_al" name="emp_al">
+                    <input required type="number" class="form-control" id="floatingInput" placeholder="emp_al" name="emp_al">
                     <label for="floatingInput">Current Annual Leave (in days)</label>
             </div>
         </div>
@@ -229,7 +157,7 @@ https://codepen.io/aniket/pen/WxByVp
 
         <div class="col-md">
             <div class="form-floating mb-3">
-                    <input type="number" class="form-control" id="floatingInput" placeholder="emp_mc" name="emp_mc">
+                    <input required type="number" class="form-control" id="floatingInput" placeholder="emp_mc" name="emp_mc">
                     <label for="floatingInput">Current Medical Leave (in days)</label>
             </div>
             <p class="card-text"><small class="text-body-secondary">
@@ -255,8 +183,7 @@ https://codepen.io/aniket/pen/WxByVp
 </div>
 
 <br>
- <button class="btn btn-default prevBtn pull-left" type="button" >Prev</button>
- <button type="submit" id="submitBtn" class="btn btn-primary" data-bs-target="#staticBackdrop">Submit</button>
+ <button type="submit" id="submitBtn" class="btn btn-primary" onclick="validateForm()">Submit</button>
 
 
 </section> 
@@ -265,174 +192,59 @@ https://codepen.io/aniket/pen/WxByVp
 </div></div>
 
 <br>
-<script>
-         document.addEventListener('DOMContentLoaded', function () {
-            // Show the modal when the form submit button is clicked
-            document.getElementById('submitBtn').addEventListener('click', function (event) {
-                event.preventDefault(); // Prevent the default form submission
-                var myModal = new bootstrap.Modal(document.getElementById('myModal'));
-                myModal.show();
-            });
-
-            // Submit the form when the modal submit button is clicked
-            document.getElementById('modalSubmitBtn').addEventListener('click', function () {
-                document.getElementById('myForm').submit();
-            });
-        });
-</script>
-<!-- Add this modal structure where your HTML content is present -->
-<div class="modal" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Confirm</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <!-- Add your modal content here -->
-                <p>Submit the registration.</p>
-            </div>
-            <div class="modal-footer">
-            <button type="button" id="modalSubmitBtn" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
-             </div>
-        </div>
-    </div>
-</div>
 
 
     </div><br>
 </div>
 
 
-</form>
-</div></div></div>
 
+</div></div></div>
+</form>
 
 </main>
 
 <script>
-   // Function for live input validation
-   function validateInput(inputElement, regex, errorElementId, errorMessage) {
-      inputElement.addEventListener('input', function () {
-        var inputValue = this.value;
 
-        // Check if the input is empty
-        if (inputValue.trim() === '') {
-          document.getElementById(errorElementId).textContent = '';
-        } else {
-          // Validate the input using the provided regex
-          if (regex.test(inputValue)) {
-            document.getElementById(errorElementId).textContent = '';
-          } else {
-            document.getElementById(errorElementId).textContent = errorMessage;
-          }
-        }
-      });
-    }
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
 
-    // Validate Name
-    validateInput(
-      document.getElementById('emp_name'),
-      /^[a-zA-Z\s]+$/,
-      'nameError',
-      'Invalid name. Please use only letters and spaces.'
-    );
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
-    // Validate Email
-    validateInput(
-      document.getElementById('emp_email'),
-      /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-      'emailError',
-      'Invalid email address.'
-    );
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
 
-    // Validate Phone Number
-validateInput(
-  document.getElementById('emp_phone'),
-  /^\d{10,}$/,
-  'phoneError',
-  'Invalid phone number. Please enter at least 10 digits.'
-);
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()</script>
 
-$(document).ready(function () {
-    var navListItems = $('div.setup-panel div a'),
-        allWells = $('.setup-content'),
-        allNextBtn = $('.nextBtn'),
-        allPrevBtn = $('.prevBtn');
 
-    allWells.hide();
-
-    navListItems.click(function (e) {
-        e.preventDefault();
-        var $target = $($(this).attr('href')),
-            $item = $(this);
-
-        if (!$item.hasClass('disabled')) {
-            navListItems.removeClass('btn-primary').addClass('btn-default');
-            $item.addClass('btn-primary');
-            allWells.hide();
-            $target.show();
-            $target.find('input:eq(0)').focus();
-        }
-    });
-
-    // Initially disable the Next button
-    allNextBtn.addClass('btn-secondary').attr('disabled', 'disabled');
-
-    allNextBtn.click(function () {
-        var curStep = $(this).closest(".setup-content"),
-            curStepBtn = curStep.attr("id"),
-            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
-            isValid = true;
-
-        $(".form-group").removeClass("has-error");
-        for (var i = 0; i < curInputs.length; i++) {
-            if (!curInputs[i].validity.valid) {
-                isValid = false;
-                $(curInputs[i]).closest(".form-group").addClass("has-error");
-            }
-        }
-
-        // Update the appearance and enable/disable the Next button
-        if (isValid) {
-            $(this).removeClass('btn-danger').addClass('btn-primary');
-            nextStepWizard.removeAttr('disabled');
-        } else {
-            $(this).removeClass('btn-primary').addClass('btn-secondary');
-            nextStepWizard.attr('disabled', 'disabled');
-        }
-    });
-
-    allPrevBtn.click(function () {
-        var curStep = $(this).closest(".setup-content"),
-            curStepBtn = curStep.attr("id"),
-            prevStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
-
-        $(".form-group").removeClass("has-error");
-        prevStepWizard.removeAttr('disabled').trigger('click');
-    });
-
-    $('div.setup-panel div a.btn-primary').trigger('click');
-});
-
-  </script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
 <script>
-    function validateAndProceed() {
-        // Get values from paragraphs
-        var name = document.getElementById('name').textContent.trim();
-        var email = document.getElementById('email').textContent.trim();
-        // Get values for other paragraphs similarly
+$(document).ready(function() {
+    // Check email duplication on keyup event
+    $('#emp_email').keyup(function() {
+        var email = $(this).val();
 
-        // Check if all required fields are filled
-        if (name !== '' && email !== '') {
-            // If all required fields are filled, proceed to the next step
-            document.getElementById('step-4').style.display = 'none'; // Hide current step
-            document.getElementById('step-5').style.display = 'block'; // Show next step
-        } else {
-            // If any required field is empty, show an alert or handle it accordingly
-            alert('Please fill in all required fields before proceeding.');
-        }
-    }
+        // Make an AJAX request to check for email duplication
+        $.ajax({
+            type: 'POST',
+            url: 'check_email.php', // Create this file to handle email checking
+            data: { email: email },
+            success: function(response) {
+                // Display the feedback
+                $('#emailFeedback').html(response);
+            }
+        });
+    });
+});
 </script>
