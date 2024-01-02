dd9e88e06dd62d5d080b0344155b842909c6d8e5
onetynine <aziziseers@gmail.com>
2024-01-02 17:55:02 +0800

    Update emp_profile.php
    next to get individual info based on id

--- emp_profile.php
+++ emp_profile.php
@@ -17,26 +17,33 @@ require "conn.php";
             <br>
         </div>
         <br>
-        <div class="card" style="max-width: 540px;">
+        <div class="card">
     <div class="row no-gutters">
 
         <div class="col-md-8">
             <!-- Right side for other texts -->
             <div class="card-body">
-                <h5 class="card-title">Employee Name</h5>
-                <p class="card-text">Designation</p>
+                <h5 class="card-title">John Doe</h5>
+                <p class="card-text">CEO</p>
                 <p class="card-text">Add Social Media buttons here for contact</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-        <div class="col-md-4">
-            <!-- Left side for profile photo -->
-            <img src="..." class="card-img" alt="Profile Photo">
+        <style>
+          .card-img{
+            border-radius: 50%;
+            width: 180px;
+            margin: 15px;
+
+          }
+        </style>
+        <div class="col-md-4 d-flex justify-content-end">
+            <img src="assets/profile.jpeg" class="card-img" alt="Profile Photo">
         </div>
     </div>
 </div>
 <br>
-        <div class="card text-center">
+        <div class="card">
   <div class="card-header">
     <ul class="nav nav-tabs card-header-tabs">
       <li class="nav-item">
