e8ba33e5d07798cc188a94316985d80c47c3c8a2
aziziseers <aziziseers@gmail.com>
2024-01-11 08:28:12 +0800

    11/1/2024
    redesigned settings (more option)

--- emp_settings.php
+++ emp_settings.php
@@ -72,7 +72,8 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <input class="form-check-input" type="checkbox" role="switch" name="emp_admin_access" id="emp_admin_access">
   <label class="form-check-label" for="emp_admin_access">Current Medical Leave</label>
 </div>
-
+<br>
+<button type="submit" class="btn btn-primary btn-sm">Save Changes</button>
 </form>
 </div>
 </div><br>
@@ -80,36 +81,64 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <div class="card">
       <div class="card-body"> 
         <h5 class="brand">More Options</h5><hr>
-        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+        <div class="row g-4 py-3 row-cols-1 row-cols-lg-3">
           
-      <div class="row d-flex align-items-start">
-        <div class="text-center">
-          <i class="fa-solid fa-people-group fa-2xl"></i>
+      <div class="row d-flex align-items-start ">
+        <div class="text-center m-4">
+          <div class="card">  
+            <span class=" text-bg-primary">Active: 4</span><br><br>
+          <i class="fa-solid fa-people-group fa-2xl"></i><br>
           <h5 class="text-body-emphasis m-2">Department</h5>
-          <a href="emp_settings_department.php" class="btn btn-primary "><i class="fa fa-gears"></i>
-          Modify
+          <div class="m-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sequi ipsa aspernatur nobis eos 
+            voluptatibus soluta pariatur fuga, magni repellat!
+          </div>
+          <a href="emp_settings_department.php" class="btn btn-primary btn-sm m-4">
+          
+          <i class="fa fa-gears"></i>
+          Modify  
           </a>
         </div>
+        </div>
       </div>
+      
       <div class="row d-flex align-items-start">
-        <div class="text-center">
-          <i class="fa-solid fa-file-signature fa-2xl"></i>
-          <h5 class="text-body-emphasis m-2">Contract Type</h5>
-          <a href="emp_settings_department.php" class="btn btn-primary "><i class="fa fa-gears"></i>
-          Modify
-          </a>
-     
-        </div>
-      </div>
-      <div class="row d-flex align-items-start">
-        <div class="text-center">
-          <i class="fa-solid fa-people-group fa-2xl"></i>
-          <h5 class="text-body-emphasis m-2">Status</h5>
-          <a href="emp_settings_department.php" class="btn btn-primary "><i class="fa fa-gears"></i>
-          Modify
+        <div class="text-center m-4">
+          <div class="card">  
+            <span class="text-bg-primary">Active: 4</span><br><br>
+          <i class="fa-solid fa-file-signature fa-2xl"></i><br>
+          <h5 class="text-body-emphasis m-2">Department</h5>
+          <div class="m-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sequi ipsa aspernatur nobis eos 
+            voluptatibus soluta pariatur fuga, magni repellat!
+          </div>
+          <a href="emp_settings_department.php" class="btn btn-primary btn-sm m-4">
+          
+          <i class="fa fa-gears"></i>
+          Modify  
           </a>
         </div>
+        </div>
       </div>
+      <div class="row d-flex align-items-start ">
+        <div class="text-center m-4">
+          <div class="card">  
+            <span class=" text-bg-primary">Active: 4</span><br><br>
+          <i class="fa-solid fa-file-signature fa-2xl"></i><br>
+          <h5 class="text-body-emphasis m-2">Department</h5>
+          <div class="m-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sequi ipsa aspernatur nobis eos 
+            voluptatibus soluta pariatur fuga, magni repellat!
+          </div>
+          <a href="emp_settings_department.php" class="btn btn-primary btn-sm m-4">
+           
+          <i class="fa fa-gears"></i>
+          Modify  
+          </a>
+        </div>
+        </div>
+      </div>
+      
     </div>
     </div>
 </div>
