02dd7c825523f96cd0f22e865304e15ff22836d2
aziziseers <aziziseers@gmail.com>
2024-01-15 08:21:47 +0800

    db

--- emsdemo.sql
+++ emsdemo.sql
@@ -1,3 +1,10 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -7,9 +14,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for ems
 CREATE DATABASE IF NOT EXISTS `ems` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `ems`;
 
+-- Dumping structure for table ems.employee
 CREATE TABLE IF NOT EXISTS `employee` (
   `emp_id` int NOT NULL AUTO_INCREMENT,
   `emp_name` varchar(255) DEFAULT NULL,
@@ -23,25 +33,16 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `emp_nric` int DEFAULT NULL,
   `emp_phone` int DEFAULT NULL,
   `emp_password` varchar(255) DEFAULT NULL,
-  `emp_admin_access` tinyint(1) DEFAULT NULL,
+  `emp_admin_access` tinyint(1) DEFAULT '0',
+  `emp_al` int DEFAULT NULL,
+  `emp_mc` int DEFAULT NULL,
   PRIMARY KEY (`emp_id`),
   KEY `emp_email` (`emp_email`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DELETE FROM `employee`;
-INSERT INTO `employee` (`emp_id`, `emp_name`, `emp_email`, `emp_designation`, `emp_department`, `emp_contract_type`, `emp_start_date`, `emp_end_date`, `emp_status`, `emp_nric`, `emp_phone`, `emp_password`, `emp_admin_access`) VALUES
-	(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(2, 'John Doe', 'john.doe@example.com', 'Manager', 'Sales', 'Full-Time', '2023-01-01', '2023-12-31', 'Active', NULL, NULL, '1234', NULL),
-	(3, 'Jane Smith', 'jane.smith@example.com', 'Engineer', 'IT', 'Part-Time', '2023-02-01', '2023-11-30', 'Inactive', NULL, NULL, NULL, NULL),
-	(4, 'Bob Johnson', 'bob.johnson@example.com', 'Analyst', 'Finance', 'Full-Time', '2023-03-01', '2023-10-31', 'Active', NULL, NULL, NULL, NULL),
-	(5, 'Alice Williams', 'alice.williams@example.com', 'Developer', 'IT', 'Full-Time', '2023-04-01', '2023-09-30', 'Active', NULL, NULL, NULL, NULL),
-	(6, 'Charlie Brown', 'charlie.brown@example.com', 'Coordinator', 'HR', 'Part-Time', '2023-05-01', '2023-08-31', 'Inactive', NULL, NULL, NULL, NULL),
-	(7, 'Eva Davis', 'eva.davis@example.com', 'Designer', 'Marketing', 'Full-Time', '2023-06-01', '2023-07-31', 'Active', NULL, NULL, NULL, NULL),
-	(8, 'David Lee', 'david.lee@example.com', 'Supervisor', 'Operations', 'Full-Time', '2023-07-01', '2023-06-30', 'Active', NULL, NULL, NULL, NULL),
-	(9, 'Grace Miller', 'grace.miller@example.com', 'Manager', 'HR', 'Part-Time', '2023-08-01', '2023-05-31', 'Inactive', NULL, NULL, NULL, NULL),
-	(10, 'Sam Wilson', 'sam.wilson@example.com', 'Analyst', 'Finance', 'Full-Time', '2023-09-01', '2023-04-30', 'Active', NULL, NULL, NULL, NULL),
-	(11, 'Linda Taylor', 'linda.taylor@example.com', 'Engineer', 'IT', 'Full-Time', '2023-10-01', '2023-03-31', 'Active', NULL, NULL, NULL, NULL);
+-- Data exporting was unselected.
 
+-- Dumping structure for table ems.leave_info
 CREATE TABLE IF NOT EXISTS `leave_info` (
   `leave_id` int NOT NULL AUTO_INCREMENT,
   `leave_name` varchar(255) DEFAULT NULL,
@@ -57,19 +58,9 @@ CREATE TABLE IF NOT EXISTS `leave_info` (
   KEY `emp_id` (`emp_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DELETE FROM `leave_info`;
-INSERT INTO `leave_info` (`leave_id`, `leave_name`, `leave_designation`, `leave_department`, `leave_start_date`, `leave_end_date`, `leave_type`, `leave_reason`, `leave_approval`, `emp_id`) VALUES
-	(1, 'John Doe', 'Manager', 'Sales', '2023-03-01', '2023-03-05', 'Vacation', 'Holiday break', 1, 1),
-	(2, 'Jane Smith', 'Engineer', 'IT', '2023-04-01', '2023-04-03', 'Sick Leave', 'Flu', 0, 2),
-	(3, 'Bob Johnson', 'Analyst', 'Finance', '2023-05-01', '2023-05-05', 'Vacation', 'Family trip', 1, 3),
-	(4, 'Alice Williams', 'Developer', 'IT', '2023-06-01', '2023-06-03', 'Personal Leave', 'Important event', 0, 4),
-	(5, 'Charlie Brown', 'Coordinator', 'HR', '2023-07-01', '2023-07-05', 'Vacation', 'Summer break', 1, 5),
-	(6, 'Eva Davis', 'Designer', 'Marketing', '2023-08-01', '2023-08-03', 'Sick Leave', 'Cold', 0, 6),
-	(7, 'David Lee', 'Supervisor', 'Operations', '2023-09-01', '2023-09-05', 'Vacation', 'Traveling', 1, 7),
-	(8, 'Grace Miller', 'Manager', 'HR', '2023-10-01', '2023-10-03', 'Personal Leave', 'Family function', 0, 8),
-	(9, 'Sam Wilson', 'Analyst', 'Finance', '2023-11-01', '2023-11-05', 'Vacation', 'Holiday trip', 1, 9),
-	(10, 'Linda Taylor', 'Engineer', 'IT', '2023-12-01', '2023-12-03', 'Sick Leave', 'Fever', 0, 10);
+-- Data exporting was unselected.
 
+-- Dumping structure for table ems.memo
 CREATE TABLE IF NOT EXISTS `memo` (
   `memo_id` int NOT NULL AUTO_INCREMENT,
   `memo_subject` varchar(255) DEFAULT NULL,
@@ -84,14 +75,39 @@ CREATE TABLE IF NOT EXISTS `memo` (
   CONSTRAINT `fk_memo_emp_id` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DELETE FROM `memo`;
-INSERT INTO `memo` (`memo_id`, `memo_subject`, `memo_content`, `memo_department`, `memo_start_date`, `memo_end_date`, `memo_post_date`, `emp_id`) VALUES
-	(1, 'Important Announcement', 'Please be informed about the upcoming meeting.', 'HR', '2023-03-01', '2023-03-01', '2023-02-25', 1),
-	(2, 'Reminder: Project Deadline', 'The project deadline is approaching. Ensure all tasks are completed.', 'IT', '2023-04-01', '2023-04-10', '2023-03-25', 2),
-	(3, 'Policy Update', 'There have been updates to the company policies. Please review the changes.', 'Finance', '2023-05-01', '2023-05-01', '2023-04-25', 3),
-	(4, 'Team Building Event', 'Get ready for the upcoming team building event on the weekend!', 'Marketing', '2023-06-01', '2023-06-02', '2023-05-25', 4),
-	(5, 'Training Session', 'There will be a mandatory training session for all employees next week.', 'Operations', '2023-07-01', '2023-07-05', '2023-06-25', 5),
-	(6, 'New Employee Introduction', 'Welcome our new team member joining us next month!', 'HR', '2023-08-01', '2023-08-01', '2023-07-25', 6);
+-- Data exporting was unselected.
+
+-- Dumping structure for table ems.opt
+CREATE TABLE IF NOT EXISTS `opt` (
+  `opt_department_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table ems.opt_department
+CREATE TABLE IF NOT EXISTS `opt_department` (
+  `opt_department_id` int NOT NULL AUTO_INCREMENT,
+  `opt_department_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`opt_department_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table ems.required_fields
+CREATE TABLE IF NOT EXISTS `required_fields` (
+  `require_id` bigint DEFAULT NULL,
+  `require_name` tinyint(1) DEFAULT '1',
+  `require_nric` tinyint DEFAULT '1',
+  `require_phone` tinyint DEFAULT '1',
+  `require_designation` tinyint DEFAULT '1',
+  `require_department` tinyint DEFAULT '1',
+  `require_contract_type` tinyint DEFAULT '1',
+  `require_status` tinyint DEFAULT '1',
+  `require_al` tinyint DEFAULT '1',
+  `require_mc` tinyint DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
