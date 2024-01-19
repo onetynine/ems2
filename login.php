14fc7ea0772fe1f97c694800b62c9f0a5edd8302
aziziseers <aziziseers@gmail.com>
2024-01-19 08:14:59 +0800

    19/1/2024

--- login.php
+++ login.php
@@ -2,19 +2,53 @@
 include "login_header.php";
 ?>
 <form action="login_process.php" class="">
-<div class="card">
-    <div class="card-title p-5">
-    Log In
-    </div>
-    <div class="card-body">
-        <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="emp_name" placeholder="emp_name" name="emp_name">
-                <label for="emp_name">Name</label>
-                <div class="invalid-feedback">
-                Please type a valid name.
-                </div>
-            </div>
-        </div>
-</div>
+    <div class="row">
+<div class="col-4 card mx-auto" style="margin-top: 20vh;">
 
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body text-center">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+    </li>
+    <li class="list-group-item">Password: </li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
 </form>
+<div class="col-8 card mx-auto w-50 border-0" style="margin-top: 20vh;">
+  <div class="card-body text-center">
+    <h5 class="card-title">EMS</h5>
+    <p class="card-text">Hi, welcome to EMS! It stands for Employee Management System. 
+    This is a demo version, some or many modules are disabled until they are completed. 
+    Please write an email or send me a message if you have any
+    suggestions to make. </p>
+    <div class="card-body">
+        <div class="row">
+        <div class="col m-6">
+                <a href="#" class=""><i class="fa-solid fa-2xl fa-unlock"></i><br><br>
+                <p>Login as Admin</p></a>
+        </div> 
+        <div class="col m-6">
+                <a href="#" class=""><i class="fa-solid fa-2xl fa-unlock"></i><br><br>
+                <p>Login as an Employee</p></a>
+        </div>
+    </div>
+  </div>
+  </div><br>
+
+
+</div>
